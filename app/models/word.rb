e959386cf0dd40f093ee5310786c0b67e674faee
Timothy Amadi <timothy@Timothys-Air.attlocal.net>
2017-10-28 11:38:40 -0400

    Add a type of word

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,5 @@
 class Word < ApplicationRecord
+  enum type: [:review_word, :challenge_word, :pattern_word, :memory_word]
   belongs_to :list
 
   validates :text, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007180239) do
+ActiveRecord::Schema.define(version: 20171028153544) do
 
   create_table "lists", force: :cascade do |t|
     t.text "contents"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171007180239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "list_id"
+    t.integer "type"
   end
 
 end
